efae9daa44b96c9074aecd2236e894c621384503
lixiang130 <1309776181@qq.com>
2022-07-11 18:56:51 +0800

    update

--- tools/auto_install/debian_init.sh
+++ tools/auto_install/debian_init.sh
@@ -37,13 +37,6 @@ if [[ $? != 0 ]]; then
 fi
 
 #出现过LspInstallInfo 中nvim-lsp-installer-errors-github-api出错的情况
-#通过安装github cli(gh)可以解决:
-#VERSION=`curl  "https://api.github.com/repos/cli/cli/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c2-`
-#curl -sSL https://github.com/cli/cli/releases/download/v${VERSION}/gh_${VERSION}_linux_amd64.tar.gz -o gh_${VERSION}_linux_amd64.tar.gz
-#tar -xzvf gh_${VERSION}_linux_amd64.tar.gz
-#sudo cp gh_${VERSION}_linux_amd64/bin/gh /usr/local/bin/
-#sudo cp -r gh_${VERSION}_linux_amd64/share/man/man1/* /usr/share/man/man1/
-#简单来说就是要安装github cli(gh),文件放在当前目录,复制到/usr/local/bin下即可
 #实际发现是因为没有安装curl:)
 sudo apt-get install curl -y
 if [[ $? != 0 ]]; then
