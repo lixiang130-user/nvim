446a50bd4a73e0efdca62fd0f22433df990275ae
xiangli130 <1309776181@qq.com>
2022-08-19 17:17:57 +0800

    mac下安装on-my-zsh自动补全等功能完整

--- tools/auto_install/macos_install.py
+++ tools/auto_install/macos_install.py
@@ -15,37 +15,16 @@ github mac passwd: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
 
 6.或许要更换主题,linux下用的nvim主题macos下可能不好看
 7.翻墙软件clashx
-    .bash_profile 或 .zshrc中写入:
+    .bash_profile 或 .zshrc中写入,下面的字符,
+    在所有需要安装的都完毕后,在最后修改追加
+    car macos_shellrc.sh >> ~/.zshrc
 
 8.安装iterm2并设置为默认终端
     brew install iterm2
     打开iterm2 菜单中点击 make iterm2 default term
-
-9.其他功能安装
-'''
-
-
-'''
-.bash_profile 或 .zshrc中写入:
-function proxy_on()
-{
-    export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
-    export http_proxy="http://127.0.0.1:7890"
-    export https_proxy=$http_proxy
-    #export all_proxy=socks5://127.0.0.1:7890 # or this line
-    echo -e "已开启代理"
-}
-
-function proxy_off()
-{
-    unset http_proxy
-    unset https_proxy
-    echo -e "已关闭代理"
-}
-
-alias vi='nvim'
-alias vim='nvim'
-alias ll='ls -al'
-export PATH=$PATH:/opt/homebrew/bin
-
+9.安装on-my-zsh 并 修改默认的shell, 这个是配合iterm2使用
+    /bin/bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    chsh -s /bin/zsh    #修改为默认的终端
+    #chsh -s /bin/bash	#修改会原来的终端
+10.其他功能安装
 '''
---
+++ tools/auto_install/macos_shellrc.sh
@@ -0,0 +1,21 @@
+
+function proxy_on()
+{
+    export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
+    export http_proxy="http://127.0.0.1:7890"
+    export https_proxy=$http_proxy
+    #export all_proxy=socks5://127.0.0.1:7890 # or this line
+    echo -e "已开启代理"
+}
+
+function proxy_off()
+{
+    unset http_proxy
+    unset https_proxy
+    echo -e "已关闭代理"
+}
+
+alias vi='nvim'
+alias vim='nvim'
+alias ll='ls -al'
+export PATH=$PATH:/opt/homebrew/bin
