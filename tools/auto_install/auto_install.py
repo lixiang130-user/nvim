0a302fbe82fbbba38c5067843472ccdd6b5072a4
lixiang130 <1309776181@qq.com>
2023-04-04 16:04:43 +0800

    man 功能重新安装,windows工具添加

--- tools/auto_install/auto_install.py
+++ tools/auto_install/auto_install.py
@@ -101,6 +101,8 @@ class apt_install(object):
         if(os.system('sudo apt-get install -y npm dos2unix ripgrep gcc') != 0):exit(-1)
         if(os.system('sudo apt-get install -y g++ gcc-multilib g++-multilib') != 0):exit(-1)
         if(os.system('sudo apt-get install -y python3-venv') != 0):exit(-1)
+        if(os.system('sudo apt-get autoremove -y man*') != 0):exit(-1)  #重新安装man,默认的不能用
+        if(os.system('sudo apt-get install -y man-db manpages-de manpages-de-dev manpages-dev glibc-doc manpages-posix-dev manpages-posix') != 0):exit(-1)
         return True
     pass
 
