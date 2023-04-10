5fc4881c5be612ad63fbe6a932c9a238dcbba3da
lixiang130 <1309776181@qq.com>
2023-04-10 11:46:50 +0800

    关闭call_station环境配置

--- tools/script/etc_profile_d_user_login_run.sh
+++ tools/script/etc_profile_d_user_login_run.sh
@@ -52,9 +52,3 @@ function set_esp32_env()
 #将终端配置\w改为\W,绝对路径->当前路径：\u 显示username,\h 显示hosename, \W 显示当前目录
 #PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u:\[\033[01;34m\]\W\[\033[00m\]\$ '
-
-function set_call_station_path()
-{
-    export MI30_V3S_TOOLCHAIN_PATH=~/tools/comtom_toolchain/mi30x/arm-linux-gnueabihf
-    export PATH=$PATH:$MI30_V3S_TOOLCHAIN_PATH/bin:$MI30_V3S_TOOLCHAIN_PATH
-}
