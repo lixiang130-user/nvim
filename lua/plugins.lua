882b1a77f9c698364f9b4d1bc785cc3515afdef9
lixiang <lixiang@comtom.cn>
2023-05-31 19:16:31 +0800

    windows terminal 修改字体, 支持neovim下各种图标的展示了

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -97,7 +97,7 @@ require('bufferline').setup {
             highlight = 'Directory',
             text_align = 'left'
         } },
-        -- separator_style =  'slant', -- 倾斜的标签
+        separator_style =  'slant', -- 倾斜的标签
         indicator = {style = 'underline'},    -- 展示下划线
     }
 }
