dbfba2f77a096df2c02354dfd803cff6ab073075
lixiang130 <1309776181@qq.com>
2024-01-08 08:28:21 +0800

    默认打开代理

--- tools/script/user_tools.sh
+++ tools/script/user_tools.sh
@@ -181,4 +181,4 @@ function git_submodule_sync()
 
 ###########################默认启动执行程序#############################
 #google_translator_vim_on   #默认打开google翻译
-#proxy_on    #开启了代理能google翻译了
+proxy_on    #开启了代理能google翻译了
