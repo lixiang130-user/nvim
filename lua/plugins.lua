45393741d3aba157ed3d6ec0dd581fe68af30235
lixiang <lixiang@comtom.cn>
2024-05-24 09:20:41 +0800

    添加几个好看的主体,修改使用的主体

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,12 +3,12 @@
 require('packer').startup(function()
     -- Packer can manage itself 包工具可以管理自己
     use 'wbthomason/packer.nvim'
-    -- gruvbox theme
-    -- use {'ellisonleao/gruvbox.nvim', requires = {'rktjmp/lush.nvim'}}
-    -- nord theme
-    -- use 'shaunsingh/nord.nvim'
-    -- zephyr-nvim theme
+    -- 好看的主体配色,最开始用的是zephyr-nvim
     use 'glepnir/zephyr-nvim'
+    use 'morhetz/gruvbox'
+    use 'nanotech/jellybeans.vim'
+    use 'tomasr/molokai'
+    use 'kabbamine/yowish.vim'
     -- treesitter 语法高亮, :TSInstallInfo查看支持的语言
     -- :TSInstall lua,安装语言解析器, :TSBufToggle highlight 可根据语法高亮显示
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
@@ -51,7 +51,11 @@ end)
 -- gruvbox nord zephyr-nvim 等主题插件配置
 vim.api.nvim_command('set background=dark') -- 设置背景色,调用vim设置的方式
 -- vim.api.nvim_command('colorscheme zephyr') -- 设置主题,调用vim设置的方式
-vim.cmd("colorscheme zephyr")
+-- vim.cmd("colorscheme zephyr")
+-- vim.cmd("colorscheme gruvbox")
+-- vim.cmd("colorscheme jellybeans")
+vim.cmd("colorscheme molokai")
+-- vim.cmd("colorscheme yowish")
 
 -- treesitter 语法高亮配置
 require 'nvim-treesitter.configs'.setup {
