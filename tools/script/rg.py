fdbb03cdc1305cc21087a555f54ea274b193751a
lixiang <lixiang@comtom.cn>
2024-06-12 17:34:47 +0800

    添加脚本自动更新ripgrep工具,修复telescope不忽略gitignore

--- tools/script/rg.py
+++ tools/script/rg.py
@@ -5,7 +5,8 @@ import sys
 if len(sys.argv) == 2 and sys.argv[1] == "user_replace":
     if not os.path.exists('/usr/bin/rg_real'):
         os.system('sudo mv /usr/bin/rg /usr/bin/rg_real')
-    os.system('sudo cp ./rg.py /usr/bin/rg')
+    ret = os.system('sudo cp ./rg.py /usr/bin/rg')
+    print("成功替换ripgrep执行文件:", ret)
     exit(0)
 
 cmd = "rg_real --no-ignore "
