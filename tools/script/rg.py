8443c4568d8c3ec3f155609e43cb9d1f6338169c
lixiang <lixiang@comtom.cn>
2024-06-17 17:54:33 +0800

    添加rg回复和替换的功能

--- tools/script/rg.py
+++ tools/script/rg.py
@@ -8,6 +8,11 @@ if len(sys.argv) == 2 and sys.argv[1] == "user_replace":
     ret = os.system('sudo cp ./rg.py /usr/bin/rg')
     print("成功替换ripgrep执行文件:", ret)
     exit(0)
+if len(sys.argv) == 2 and sys.argv[1] == "user_recover":
+    if not os.path.exists('/usr/bin/rg_real'):
+        exit(0)
+    os.system('sudo mv /usr/bin/rg_real /usr/bin/rg ')
+    exit(0)
 
 cmd = "rg_real --no-ignore "
 for i in sys.argv[1:]:
