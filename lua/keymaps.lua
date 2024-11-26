8fd9a1ddae1d5df91a3aed9a52b5a71323e3141c
lixiang <lixiang@comtom.cn>
2024-11-26 09:28:32 +0800

    1.utf8转码成bom的, 2修改mi30环境变量路径

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -23,10 +23,10 @@ local map = vim.api.nvim_set_keymap
 local opt = { noremap = true, silent = true }
 
 -- 配置普通模式单个案件进行上下翻半页,一页的按键映射 设置普通模式下的箭头键映射
-vim.keymap.set('n', '<Up>',     '<C-u>')
-vim.keymap.set('n', '<Down>',   '<C-d>')
-vim.keymap.set('n', '<Left>',   '<C-b>')
-vim.keymap.set('n', '<Right>',  '<C-f>')
+-- vim.keymap.set('n', '<Up>',     '<C-u>')
+-- vim.keymap.set('n', '<Down>',   '<C-d>')
+-- vim.keymap.set('n', '<Left>',   '<C-b>')
+-- vim.keymap.set('n', '<Right>',  '<C-f>')
 
 
 -- bufferline 跳转配置
