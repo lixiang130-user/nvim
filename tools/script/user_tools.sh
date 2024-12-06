3d66dfdda02520a38c05cd17dad552d40fb10695
lixiang <lixiang@comtom.cn>
2024-12-06 09:58:08 +0800

    修改cdd路径

--- tools/script/user_tools.sh
+++ tools/script/user_tools.sh
@@ -448,6 +448,6 @@ function mount_sshfs()
 #proxy_on    #开启了代理能google翻译了
 path_simplify
 #常用的临时自定义工作目录,修改路径,直接进入到这个目录里
-#alias cdd='cd ~/linux/work/20/h6010 ; echo `pwd`'
-alias cdd='cd ~/linux/work/81/ped_call-station ; echo `pwd`'
+tmp_cdd_path=~/linux/temp/lixiang/linux/videointercom
+alias cdd='cd $tmp_cdd_path ; echo `pwd`'
 
