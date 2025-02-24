72ba84553ce51c820016108e51d40545f081e3bc
lixiang <lixiang@comtom.cn>
2025-02-24 16:44:55 +0800

    添加编译环境安装

--- tools/auto_install/auto_install.py
+++ tools/auto_install/auto_install.py
@@ -391,7 +391,7 @@ elif sys.argv[1] == 'work': #工作需要的环境
     print('安装工作环境 行号:', sys._getframe().f_lineno, '返回值:', ret)
     ret = os.system('sudo ln -sf /bin/bash /bin/sh')
     print('安装工作环境 行号:', sys._getframe().f_lineno, '返回值:', ret)
-    ret = os.system('sudo apt-get install autoconf automake libtool')
+    ret = os.system('sudo apt-get install autoconf automake libtool texinfo')
     print('安装工作环境 行号:', sys._getframe().f_lineno, '返回值:', ret)
 elif sys.argv[1] == 'bc': #工作需要的环境
     bc = bcompare_install()
