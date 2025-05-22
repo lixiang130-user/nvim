26deebc9cea106aaee0677c0415dfc8e6447355b
lixiang <lixiang@comtom.cn>
2025-05-22 09:02:59 +0800

    更新脚本

--- tools/script/expect/expect_nvim.sh
+++ tools/script/expect/expect_nvim.sh
@@ -10,12 +10,15 @@ eval spawn nvim_code $args
 
 # 等待一会，输入命令
 expect ".*"
-send ":sp\r"
-send ":terminal\r"
-send ":vsp\r"
-send ":terminal\r"
-send "6w-\r"
-send "11w,\r"
+#打开两个默认的终端
+#send ":sp\r"
+#send ":terminal\r"
+#send ":vsp\r"
+#send ":terminal\r"
+#send "6w-\r"
+#send "11w,\r"
+##在linux下面的组合键实际就是ASCII,看看任意键值:showkey -a; ctrl+h 是:0010
+#send "\013\r"
 
 # 保持交互状态
 interact
